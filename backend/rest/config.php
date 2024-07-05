42782c2b03dc2456cce6d590203936f8e9b3e72f
MirsadHadzic <mirsad.hadzic.01@gmail.com>
2024-07-05 16:43:49 +0200

    za deplotment

--- backend/rest/config.php
+++ backend/rest/config.php
@@ -6,22 +6,22 @@ error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
 class Config {
     public static function DB_NAME() {
-        return Config::get_env("DB_NAME", "webfinal"); // webfinal
+        return Config::get_env("DB_NAME", ""); // webfinal
     }
     public static function DB_PORT() {
-        return Config::get_env("DB_PORT", 25060); // 3306
+        return Config::get_env("DB_PORT", ); // 3306
     }
     public static function DB_USER() {
-        return Config::get_env("DB_USER", "doadmin"); // root
+        return Config::get_env("DB_USER", ""); // root
     }
     public static function DB_PASSWORD() {
-        return Config::get_env("DB_PASSWORD", "AVNS_gs32YDhMw0hAJEXUnRH"); // root
+        return Config::get_env("DB_PASSWORD", ""); // root
     }
     public static function DB_HOST() {
-        return Config::get_env("DB_HOST", "db-mysql-fra1-55514-do-user-14099042-0.a.db.ondigitalocean.com"); // localhost
+        return Config::get_env("DB_HOST", ""); // localhost
     }
     public static function JWT_SECRET() {
-        return Config::get_env("JWT_SECRET", "mehagamehaga"); // mehagamehaga
+        return Config::get_env("JWT_SECRET", ""); // mehagamehaga
     }
     public static function get_env($name, $default) {
         return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
